470bed38b13cbd8ec2d8b685cc80f7b64b29e69b
Filat Astakhov <astakhovfilat@gmail.com>
2019-01-27 21:53:14 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+
+
+bot_token = os.environ['BOT_TOKEN']
+chat_id = os.environ['CHAT_ID']
+group_id = os.environ['GROUP_ID']
---
+++ main.py
@@ -0,0 +1,38 @@
+import vk_api, config
+from vk_api.keyboard import VkKeyboard
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.utils import get_random_id
+
+
+def main():
+
+    vk_session = vk_api.VkApi(token=str(config.bot_token))
+    vk = vk_session.get_api()
+
+    long_poll = VkBotLongPoll(vk_session, int(config.group_id))
+
+    keyboard = VkKeyboard(one_time=False)
+    keyboard.add_button(label="70 руб.", color="primary")
+    keyboard.add_button(label="80 руб.", color="primary")
+    keyboard.add_button(label="90 руб.", color="primary")
+
+    vk.messages.send(
+        peer_id=(str(config.chat_id)),
+        keyboard=keyboard.get_keyboard(),
+        random_id=get_random_id(),
+        message='Привет, выбери стоимость заказа'
+    )
+
+    for event in long_poll.listen():
+        if event:
+            print(event)
+
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            if event.object['peer_id'] and event.object['peer_id'] == str(config.chat_id):
+                print('id{} : {}'.format(event.obj.from_id, event.obj.text), end='\n')
+
+
+if __name__ == '__main__':
+    print("Bot started")
+    main()
+
